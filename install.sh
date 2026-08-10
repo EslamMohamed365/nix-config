1724b40214429ff90e754b92298f9c2c8f525b59
EslamMohamed365 <am5484452@gmail.com>
2026-08-10 22:15:22 +0300

    fix: correct host name

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo nix run --extra-experimental-features "nix-command flakes" \
   'github:nix-community/disko/latest#disko-install' \
-  -- --flake github:eslam/nix-config#nix-btw \
+  -- --flake github:EslamMohamed365/nix-config#nix-btw \
   --disk main /dev/nvme0n1 \
   --write-efi-boot-entries \
   --accept-flake-config
