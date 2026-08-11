fe15419f88cabc7a83c910582285c82b4d0ef877
EslamMohamed365 <am5484452@gmail.com>
2026-08-11 07:23:49 +0300

    feat: add nh rebuild tool

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,5 +20,11 @@
   programs = {
     neovim.enable = true;
     git.enable = true;
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "/home/eslam/nix-config/"; # sets NH_OS_FLAKE variable for you
+    };
   };
 }
