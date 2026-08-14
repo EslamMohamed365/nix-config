855ebebda00fe2953e6bb12d07f715f1f4e8d0d2
EslamMohamed365 <am5484452@gmail.com>
2026-08-14 23:43:13 +0300

    ✨ feat: enhance nvf with more plugins and LSP polish

--- home-manager/modules/nvf.nix
+++ home-manager/modules/nvf.nix
@@ -11,15 +11,29 @@
         registers = "unnamedplus";
         providers.wl-copy.enable = true;
       };
-      # Built-in IDE features
-      statusline.lualine.enable = true;
-      telescope.enable = true; # Fuzzy finder
+
+      # --- UI & layout ---
+      statusline.lualine.enable = true; # Themed via Stylix by nvf
+      tabline.nvimBufferline.enable = true; # Buffer tabs with close/pick/sort
+      ui = {
+        noice.enable = true; # Pretty cmdline + notifications
+        illuminate.enable = true; # Highlight word under cursor
+      };
+
+      # --- Fuzzy finder (default binds: <leader>ff/fg/fb/fh/fr) ---
+      telescope.enable = true;
       autocomplete.nvim-cmp.enable = true; # Autocompletion
       filetree.neo-tree.enable = true; # File explorer
       binds = {
         whichKey.enable = true;
-        hardtime-nvim.enable = true;
+        hardtime-nvim.enable = true; # No repeating j/k; forces heading-style motion
       };
+
+      # --- Navigation & editing ---
+      navigation.harpoon.enable = true; # <leader>a to mark files, then <C-j/k/l/> to jump
+      utility.motion.flash-nvim.enable = true; # s/S for jump-to-anywhere motion
+      mini.ai.enable = true; # Text objects: i( i" i[ i{ ...
+      mini.indentscope.enable = true; # Indent scope guides
 
       # Core editor ergonomics
       undoFile.enable = true; # Persistent undo across restarts
@@ -34,21 +48,23 @@
       terminal.toggleterm.enable = true; # Better integrated terminal
       utility.oil-nvim.enable = true; # Modern file manager
       utility.surround.enable = true; # ys/cs/ds to edit surrounding pairs
-      ui.noice.enable = true; # Pretty cmdline + notifications
+      notes.todo-comments.enable = true; # Highlight TODO/FIXME/HACK
 
       # Language Servers & Treesitter
       lsp = {
         enable = true;
         formatOnSave = true;
+        inlayHints.enable = true; # Type hints inline
+        lspkind.enable = true; # Icons in completion menu
+        lspSignature.enable = true; # Signature help popup
+        trouble.enable = true; # <leader>ld / <leader>lwd diagnostics
       };
       languages = {
         enableTreesitter = true;
 
         # Turn on languages you use:
-        nix.enable = true;
-        tsx.enable = true; # TypeScript/JS
+        nix.enable = true; # Formatted with alejandra on save
         python.enable = true;
-        rust.enable = true;
         markdown.enable = true;
         bash.enable = true;
         lua.enable = true;
@@ -57,3 +73,4 @@
     };
   };
 }
+
