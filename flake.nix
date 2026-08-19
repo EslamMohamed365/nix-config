4b8f71f806fee872ed7de966a8067b378cb9b3a3
EslamMohamed365 <am5484452@gmail.com>
2026-08-19 16:11:53 +0300

    🏗️ refactor: migrate flake to flake-parts

--- flake.nix
+++ flake.nix
@@ -2,59 +2,51 @@
   description = "eslam's NixOS configuration";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home manager
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Declarative disk partitioning
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # NVF
+
     nvf = {
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Stylix
+
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Firefox addons
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    nixpkgs,
-    stylix,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild switch --flake .#nix-btw'
-    # or during install: 'nixos-install --flake .#nix-btw'
-    nixosConfigurations = {
-      nix-btw = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit inputs;};
-        # > Our main nixos configuration file <
-        modules = [
-          stylix.nixosModules.stylix
-          "${nixpkgs}/nixos/modules/hardware/facter"
-          ./nixos/configuration.nix
-          {hardware.facter.reportPath = ./facter.json;}
-        ];
+  outputs = inputs@{ flake-parts, nixpkgs, stylix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      flake.nixosConfigurations = {
+        nix-btw = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            stylix.nixosModules.stylix
+            "${nixpkgs}/nixos/modules/hardware/facter"
+            ./nixos/configuration.nix
+            { hardware.facter.reportPath = ./facter.json; }
+          ];
+        };
       };
     };
-  };
 }
