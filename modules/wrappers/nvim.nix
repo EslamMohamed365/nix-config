f008f98624274f0c9a63238038d70c4e80d0e006
EslamMohamed365 <am5484452@gmail.com>
2026-08-20 18:46:10 +0300

    make pure config neovim

--- modules/wrappers/nvim.nix
+++ modules/wrappers/nvim.nix
@@ -1,110 +1,172 @@
-{ pkgs, ... }:
+{pkgs, ...}: let
+  initVim = ''
+    " ========================================
+    " Options
+    " ========================================
 
-let
-  langServers = with pkgs; [
-    nil # nix
-    pyright # python
-    yaml-language-server # yaml
-    dockerfile-language-server # dockerfile
-    taplo # toml
-    lua-language-server # lua
-    bash-language-server # sh
-  ];
+    set encoding=UTF-8
+    set nohlsearch
+    set number
+    set mouse=a
+    set breakindent
+    set undofile
+    set ignorecase
+    set smartcase
+    set signcolumn=yes
+    set updatetime=250
+    set timeoutlen=300
+    set nobackup
+    set nowritebackup
+    set completeopt=menuone,noselect
+    set whichwrap+=<,>,[,],h,l
+    set nowrap
+    set linebreak
+    set scrolloff=8
+    set sidescrolloff=8
+    set relativenumber
+    set numberwidth=4
+    set shiftwidth=4
+    set tabstop=4
+    set softtabstop=4
+    set expandtab
+    set nocursorline
+    set splitbelow
+    set splitright
+    set noswapfile
+    set smartindent
+    set showtabline=2
+    set backspace=indent,eol,start
+    set pumheight=10
+    set conceallevel=0
+    set fileencoding=utf-8
+    set cmdheight=1
+    set autoindent
+    set shortmess+=c
+    set iskeyword+=-
+    set showmatch
+    set laststatus=2
+    set statusline=%f
+    set statusline+=%=
+    set statusline+=%l
+    set statusline+=/
+    set statusline+=%L
 
-  formatters = with pkgs; [
-    shfmt # shell
-    nixfmt # nix
-    stylua # lua
-    ruff # python
-  ];
+    " ========================================
+    " Keymaps
+    " ========================================
 
-  treesitterGrammars = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: with p; [
-    nix python yaml dockerfile toml lua bash
-  ]);
+    let mapleader = " "
+    let maplocalleader = " "
 
-  initLua = ''
-    vim.g.mapleader = " "
+    nnoremap <Space> <Nop>
+    vnoremap <Space> <Nop>
 
-    local opt = vim.opt
-    opt.number = true
-    opt.relativenumber = true
-    opt.mouse = "a"
-    opt.clipboard = "unnamedplus"
-    opt.termguicolors = true
-    opt.signcolumn = "yes"
-    opt.updatetime = 250
-    opt.undofile = true
-    opt.ignorecase = true
-    opt.smartcase = true
-    opt.splitright = true
-    opt.splitbelow = true
-    opt.scrolloff = 8
-    opt.completeopt = "menu,menuone,noselect"
+    nnoremap <expr> k v:count == 0 ? 'gk' : 'k'
+    nnoremap <expr> j v:count == 0 ? 'gj' : 'j'
 
-    local map = vim.keymap.set
-    map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
-    map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
-    map("n", "<Esc>", "<cmd>nohlsearch<cr>")
-    map("n", "<C-h>", "<C-w>h")
-    map("n", "<C-j>", "<C-w>j")
-    map("n", "<C-k>", "<C-w>k")
-    map("n", "<C-l>", "<C-w>l")
-    map("n", "<S-h>", "<cmd>tabprevious<cr>")
-    map("n", "<S-l>", "<cmd>tabnext<cr>")
-    map("n", "<leader>e", "<cmd>Explore<cr>", { desc = "Files" })
-    map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buf" })
-    map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev buf" })
+    nnoremap <Esc> :noh<CR>
 
-    -- Snippets (built-in vim.snippet)
-    local snip = vim.snippet
-    map({ "i", "n" }, "<C-j>", function() snip.jump(1) end, { desc = "Snippet next" })
-    map({ "i", "n" }, "<C-k>", function() snip.jump(-1) end, { desc = "Snippet prev" })
+    nnoremap <C-s> :w<CR>
+    nnoremap <leader>sn :noautocmd w<CR>
+    nnoremap <C-q> :q<CR>
 
-    -- LSP
-    vim.lsp.enable({ "nil_ls", "pyright", "yamlls", "dockerls", "taplo", "lua_ls", "bashls" })
+    nnoremap x "_x
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        local b = { buffer = args.buf }
-        map("n", "gd", vim.lsp.buf.definition, b)
-        map("n", "gr", vim.lsp.buf.references, b)
-        map("n", "gI", vim.lsp.buf.implementation, b)
-        map("n", "K", vim.lsp.buf.hover, b)
-        map("n", "<leader>ca", vim.lsp.buf.code_action, b)
-        map("n", "<leader>rn", vim.lsp.buf.rename, b)
-        map("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, b)
-        map("n", "[d", vim.diagnostic.goto_prev, b)
-        map("n", "]d", vim.diagnostic.goto_next, b)
-      end,
-    })
+    nnoremap <C-d> <C-d>zz
+    nnoremap <C-u> <C-u>zz
 
-    -- Format on save
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function()
-        vim.lsp.buf.format({ async = false, timeout_ms = 2000 })
-      end,
-    })
+    nnoremap n nzzzv
+    nnoremap N Nzzzv
 
-    -- Treesitter
-    opt.runtimepath:append("${treesitterGrammars}")
-    vim.api.nvim_create_autocmd("BufEnter", {
-      callback = function()
-        pcall(vim.treesitter.start)
-      end,
-    })
+    nnoremap <Up> :resize -2<CR>
+    nnoremap <Down> :resize +2<CR>
+    nnoremap <Left> :vertical resize -2<CR>
+    nnoremap <Right> :vertical resize +2<CR>
+
+    nnoremap <Tab> :bnext<CR>
+    nnoremap <S-Tab> :bprevious<CR>
+    nnoremap <leader>sb :buffers<CR>:buffer<Space>
+
+    nnoremap <leader>+ <C-a>
+    nnoremap <leader>- <C-x>
+
+    nnoremap <leader>v <C-w>v
+    nnoremap <leader>h <C-w>s
+    nnoremap <leader>se <C-w>=
+    nnoremap <leader>xs :close<CR>
+
+    nnoremap <C-k> :wincmd k<CR>
+    nnoremap <C-j> :wincmd j<CR>
+    nnoremap <C-h> :wincmd h<CR>
+    nnoremap <C-l> :wincmd l<CR>
+
+    nnoremap <leader>to :tabnew<CR>
+    nnoremap <leader>tx :tabclose<CR>
+    nnoremap <leader>tn :tabn<CR>
+    nnoremap <leader>tp :tabp<CR>
+
+    nnoremap <leader>x :bdelete<CR>
+    nnoremap <leader>b :enew<CR>
+
+    nnoremap <leader>lw :set wrap!<CR>
+
+    inoremap jk <ESC>
+    inoremap kj <ESC>
+
+    vnoremap p "_dP
+
+    noremap <leader>y "+y
+    noremap <leader>Y "+Y
+
+    noremap <silent> <leader>e :Lex<CR>
+
+    " ========================================
+    " Other
+    " ========================================
+
+    syntax on
+
+    " colorscheme industry
+    colorscheme habamax
+    set background=dark
+
+    if system('uname -s') == "Darwin\n"
+      set clipboard=unnamed
+    else
+      set clipboard=unnamedplus
+    endif
+
+    if !has('gui_running') && &term =~ '\%(screen\|tmux\)'
+      let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+      let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+    endif
+    set termguicolors
+
+    let &t_SI = "\e[6 q"
+    let &t_EI = "\e[2 q"
+
+    " Netrw
+    let g:netrw_banner = 0
+    let g:netrw_liststyle = 3
+    let g:netrw_browse_split = 4
+    let g:netrw_altv = 1
+    let g:netrw_winsize = 25
+
+    augroup netrw_setup | au!
+        au FileType netrw nmap <buffer> l <CR>
+    augroup END
   '';
 in
-pkgs.symlinkJoin {
-  name = "nvim";
-  paths = [ pkgs.neovim ];
-  buildInputs = [ pkgs.makeWrapper ];
-  postBuild = ''
-    wrapProgram $out/bin/nvim \
-      --add-flags "-u ${pkgs.writeText "init.lua" initLua}" \
-      --prefix PATH : ${pkgs.lib.makeBinPath (langServers ++ formatters)}
-  '';
-  meta = {
-    description = "Minimal productive Neovim for nix/python/yaml/docker/toml/lua/shell";
-    mainProgram = "nvim";
-  };
-}
+  pkgs.symlinkJoin {
+    name = "nvim";
+    paths = [pkgs.neovim];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/nvim \
+        --add-flags "-u ${pkgs.writeText "init.vim" initVim}"
+    '';
+    meta = {
+      description = "Neovim with custom config";
+      mainProgram = "nvim";
+    };
+  }
