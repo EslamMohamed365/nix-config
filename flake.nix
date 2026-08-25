9818f8c396567988572080ba3126b6c03f31c3f9
EslamMohamed365 <am5484452@gmail.com>
2026-08-25 19:54:25 +0300

    fix flake.nix inputs nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    import-tree = {
-      url = "github:vic/import-tree";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
